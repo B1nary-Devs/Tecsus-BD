26603422c26f0a0449d2b15bbab7f26fd98be62a
Marcelo Silva <101959064+marcelosilva7@users.noreply.github.com>
2024-04-25 21:48:04 -0300

    <feat> BT-17
    modelo do banco agua estrela

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -2,15 +2,82 @@
 update mysql.user set host='%' where user='root';
 FLUSH PRIVILEGES;
 
---SQL modo estrela
-CREATE TABLE dim_agua_contrato (
-    Numero_Contrato SERIAL PRIMARY KEY,
-    Nome_do_Contrato VARCHAR(255),
-    Fornecedor VARCHAR(255),
-    Modalidade VARCHAR(255),
-    Forma_de_Pagamento VARCHAR(255),
-    Tipo_de_Acesso VARCHAR(255),
-    Vigencia_Inicial DATE,
-    Vigencia_Final DATE,
-    Observacao TEXT
+-- dimensão cliente
+create table dim_agua_cliente (
+    numero_cliente varchar(100) primary key, --
+    numero_contrato varchar(100), --
+    nome_cliente varchar(255),  --
+    cnpj varchar(14), --
+    tipo_de_consumidor varchar(50), -- 
+    modelo_de_faturamento varchar(255),
+    foreign key (numero_contrato) references dim_agua_contrato(numero_contrato)
 );
+
+-- dimensão contrato
+create table dim_agua_contrato (
+    numero_contrato varchar(100) primary key, --
+    nome_do_contrato varchar(255), --
+    fornecedor varchar(255), --
+    forma_de_pagamento varchar(50), --
+    tipo_de_acesso varchar(50), --
+    vigencia_inicial_id date, --
+    vigencia_final_id date, --
+    foreign key (vigencia_inicial) references dim_tempo(data_id),
+	foreign key (vigencia_final) references dim_tempo(data_id)
+);
+
+-- dimensão medidor
+create table dim_agua_medidor (
+    numero_medidor varchar(100) primary key, -- 
+    hidrometro varchar(255), --
+    codigo_de_ligacao_rgi varchar(50), --
+    numero_contrato varchar(255), --
+	endereco_de_instalacao text, --
+    numero_cliente varchar(100),
+    foreign key (numero_cliente) references dim_agua_cliente(numero_cliente),
+    foreign key (numero_contrato) references dim_agua_contrato(numero_contrato)
+);
+
+create table dim_tempo (
+    data_id date primary key,
+    dia int,
+    mes int,
+    ano int,
+    trimestre int,
+    semestre int,
+    dia_da_semana varchar(10),
+    mes_nome varchar(15)
+);
+
+-- fato consumo
+create table fato_agua_consumo (
+    fato_agua_id int auto_increment primary key,
+    planta varchar(255),
+    conta_do_mes varchar(255),
+    serie_da_nota_fiscal varchar(50),
+    numero_nota_fiscal varchar(255),
+    codigo_de_barras varchar(255),
+    chave_de_acesso varchar(255),
+    consumo_de_agua_m3 int,
+    consumo_de_esgoto_m3 int,
+    valor_agua float,
+    valor_esgoto float,
+    total_r float,
+    nivel_de_informacoes_da_fatura varchar(255),
+    multa_ref_vcto float,
+    juros_de_mora_ref_vcto float,
+    atualizacao_monetaria_ref_vcto float,
+    numero_cliente varchar(100),
+    numero_medidor varchar(100),
+    numero_contrato varchar(100),
+    vencimento_id date,
+	emissao_id date,
+	leitura_anterior_id date,
+	leitura_atual_id date,
+	foreign key (vencimento_id) references dim_tempo(data_id),
+	foreign key (emissao_id) references dim_tempo(data_id),
+	foreign key (leitura_anterior_id) references dim_tempo(data_id),
+	foreign key (leitura_atual_id) references dim_tempo(data_id),
+    foreign key (numero_cliente) references dim_agua_cliente(numero_cliente),
+    foreign key (numero_medidor) references dim_agua_medidor(numero_medidor),
+    foreign key (numero_contrato) references dim_agua_contrato(numero_contrato));
